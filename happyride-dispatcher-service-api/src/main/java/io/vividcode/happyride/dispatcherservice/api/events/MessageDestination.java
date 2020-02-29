545466f3c3f4f1f43171749c98ad94f65380e969
Fu Cheng <alexcheng1982@gmail.com>
2020-02-29 15:25:21 +1300

    trip

--- happyride-dispatcher-service-api/src/main/java/io/vividcode/happyride/dispatcherservice/api/events/MessageDestination.java
+++
@@ -1,7 +0,0 @@
-package io.vividcode.happyride.dispatcherservice.api.events;
-
-public class MessageDestination {
-  private MessageDestination() {}
-
-  public static final String TRIP_DISPATCHED = "io.vividcode.happyride.dispatcherservice.TripDispatched";
-}
