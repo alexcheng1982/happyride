342500a36834bc62d346a4e154c917bffb0ef0bc
Fu Cheng <alexcheng1982@gmail.com>
2020-03-04 03:33:08 +1300

    test

--- happyride-passenger-service/src/test/java/io/vividcode/happyride/passengerservice/TestApplication.java
+++
@@ -1,8 +0,0 @@
-package io.vividcode.happyride.passengerservice;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TestApplication {
-
-}
