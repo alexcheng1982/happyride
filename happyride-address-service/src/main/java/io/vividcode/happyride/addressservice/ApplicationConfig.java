9f4f3ad44eee44d87504c602531845bcdf67de81
Fu Cheng <alexcheng1982@gmail.com>
2020-06-16 23:30:10 +1200

    update metrics

--- happyride-address-service/src/main/java/io/vividcode/happyride/addressservice/ApplicationConfig.java
+++ happyride-address-service/src/main/java/io/vividcode/happyride/addressservice/ApplicationConfig.java
@@ -6,18 +6,22 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
+  @Bean
   public TimedAspect timedAspect(final MeterRegistry meterRegistry) {
     return new TimedAspect(meterRegistry);
+  }
 
   @Bean
-  MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer() {
+  public MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer() {
     return registry -> registry.config().commonTags("service", "address");
   }
 }
--- happyride-address-service/src/main/java/io/vividcode/happyride/addressservice/service/AddressService.java
+++ happyride-address-service/src/main/java/io/vividcode/happyride/addressservice/service/AddressService.java
@@ -14,7 +14,7 @@ public class AddressService {
   @Autowired
   AddressRepository addressRepository;
 
-  @Timed(value = "address.search", percentiles = {0.5, 0.75, 0.9})
+  @Timed(value = "happyride.address.search", percentiles = {0.5, 0.75, 0.9})
   public List<AddressView> search(final Long areaCode, final String query) {
     return this.addressRepository.findByAreaAreaCodeAndAddressLineContains(areaCode, query)
         .stream().map(AddressView::fromAddress).collect(Collectors.toList());
