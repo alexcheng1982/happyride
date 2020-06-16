0d59f4261e0c1e0ae026c4a1227362299557562f
Fu Cheng <alexcheng1982@gmail.com>
2020-06-16 17:39:09 +1200

    add metrics

--- happyride-address-service/src/main/java/io/vividcode/happyride/addressservice/ApplicationConfig.java
+++ happyride-address-service/src/main/java/io/vividcode/happyride/addressservice/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package io.vividcode.happyride.addressservice;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -8,5 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories
 @EnableTransactionManagement
 public class ApplicationConfig {
-
+  public TimedAspect timedAspect(final MeterRegistry meterRegistry) {
+    return new TimedAspect(meterRegistry);
+  }
 }
--- happyride-address-service/src/main/java/io/vividcode/happyride/addressservice/service/AddressService.java
+++ happyride-address-service/src/main/java/io/vividcode/happyride/addressservice/service/AddressService.java
@@ -1,10 +1,12 @@
 package io.vividcode.happyride.addressservice.service;
 
+import io.micrometer.core.annotation.Timed;
 import io.vividcode.happyride.addressservice.dataaccess.AddressRepository;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressService {
@@ -12,8 +14,9 @@ public class AddressService {
   @Autowired
   AddressRepository addressRepository;
 
-  public List<AddressView> search(Long areaCode, String query) {
-    return addressRepository.findByAreaAreaCodeAndAddressLineContains(areaCode, query)
+  @Timed(value = "address.search", percentiles = {0.5, 0.75, 0.9})
+  public List<AddressView> search(final Long areaCode, final String query) {
+    return this.addressRepository.findByAreaAreaCodeAndAddressLineContains(areaCode, query)
         .stream().map(AddressView::fromAddress).collect(Collectors.toList());
   }
 }
