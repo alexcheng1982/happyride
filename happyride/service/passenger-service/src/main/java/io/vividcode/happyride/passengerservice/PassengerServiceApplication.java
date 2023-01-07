f72f66ffef496eb48ec65c8f3f4dec95b448d737
Fu Cheng <alexcheng1982@gmail.com>
2023-01-07 19:35:14 +0800

    Update passenger and driver service

--- happyride/service/passenger-service/src/main/java/io/vividcode/happyride/passengerservice/PassengerServiceApplication.java
+++ happyride/service/passenger-service/src/main/java/io/vividcode/happyride/passengerservice/PassengerServiceApplication.java
@@ -1,8 +1,11 @@
 package io.vividcode.happyride.passengerservice;
 
+import io.vividcode.happyride.common.DatabaseRuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
+@ImportRuntimeHints(DatabaseRuntimeHintsRegistrar.class)
 @SpringBootApplication
 public class PassengerServiceApplication {
 
